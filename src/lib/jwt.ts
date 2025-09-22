e34489d05f38586043c0e4b7f426b9610ee906c4
Millborne <millborne.galamiton@supportzebra.com>
2025-09-22 12:11:49 +0800

    Update jwt.ts

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -4,8 +4,8 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
 
 export const generateAccessToken = (payload: object) => {
-    // return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
-    return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "1m" });
+    return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+    // return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "1m" });
 };
 
 export const generateRefreshToken = (payload: object) => {
